6d1263da1834dc24a0c9733162258fb32842650b
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-09-27 20:15:07 +0530

    added logs

--- src/main/java/com/thirdeye3/usermanager/dtos/ThresholdDto.java
+++ src/main/java/com/thirdeye3/usermanager/dtos/ThresholdDto.java
@@ -1,12 +1,6 @@
 package com.thirdeye3.usermanager.dtos;
 
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
-
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,13 +11,18 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ThresholdDto {
+
     private Long id;
 
     private ThresholdGroupDto thresholdGroup;
 
     @NotNull(message = "Time gap is required")
-    @Positive(message = "Time gap must be a positive number")
     private Long timeGapInSeconds;
+
+    @AssertTrue(message = "Time gap must be positive, Yesterday's closing price, or Today's opening price")
+    private boolean isValidTimeGap() {
+        return timeGapInSeconds != null && (timeGapInSeconds > 0 || timeGapInSeconds == -1 || timeGapInSeconds == -2);
+    }
 
     @NotNull(message = "Price gap is required")
     @DecimalMin(value = "-999999999.99", message = "Price gap too small")
