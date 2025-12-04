9761a2a992420ae8542ab94e198d6872a0b11db7
Kartikey2001nitrr <kartikeythawait123@gmail.com>
2025-12-04 23:21:17 +0530

    updated external api calls

--- src/main/java/com/thirdeye3/usermanager/filters/ApiKeyFilter.java
+++ src/main/java/com/thirdeye3/usermanager/filters/ApiKeyFilter.java
@@ -17,20 +17,26 @@ public class ApiKeyFilter extends OncePerRequestFilter {
 
     @Value("${thirdeye.api.key}")
     private String apiKey;
-    
-    @Value("${spring.profiles.active}")
-    private String profile;
-    
+
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        return path.startsWith("/api/statuschecker");
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        String requestUrl = request.getRequestURL().toString();
 
         String requestApiKey = request.getHeader("THIRDEYE-API-KEY");
-        if ((apiKey != null && apiKey.equals(requestApiKey)) || profile.equalsIgnoreCase("LOCAL")) {
+        if (requestApiKey == null) {
+            requestApiKey = request.getParameter("THIRDEYE-API-KEY");
+        }
+
+        if (apiKey != null && apiKey.equals(requestApiKey)) {
             filterChain.doFilter(request, response);
         } else {
             sendUnauthorizedResponse(response);
